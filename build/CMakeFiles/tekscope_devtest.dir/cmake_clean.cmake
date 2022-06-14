7926bca165748b8657c10c7b7a8c57bc569eff90
Arkady Lokhovitskiy <arkady.lokhovitskiy@cern.ch>
2022-06-14 15:37:53 +0200

    Starting summer program

---
+++ build/CMakeFiles/tekscope_devtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tekscope_devtest.dir/test/tekscope_devtest.cpp.o"
+  "tekscope_devtest.pdb"
+  "tekscope_devtest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tekscope_devtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /nfshome0/alokhovi/run3/brildaq-nivisa
+# Build directory: /nfshome0/alokhovi/run3/brildaq-nivisa/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(TEKSCOPE "tekscope_devtest")
+SET_TESTS_PROPERTIES(TEKSCOPE PROPERTIES  ENVIRONMENT "BOOST_TEST_LOG_LEVEL=message")
