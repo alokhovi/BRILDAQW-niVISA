b4188cd699c5d9221c9449fbe719f0509644a721
John Sledge Iv <jsledgei@srv-s2d16-26-01.cms>
2022-06-29 17:45:54 +0200

    initial binary readout sucess

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "tekscope.hpp"
 #include "tekscopecfg.hpp"
 
-
+/*
 int main()
 {
   brildaq::nivisa::TekScope    scope;
@@ -58,6 +58,8 @@ int main()
   scope.baseConfig(scopeCfg.globalParams, scopeCfg.channelConfigurationParameters);
   data = scope.query(const_cast<ViString>("HORizontal:SAMPLERate?"));
   std::cout << "SAMPLE RATE: " << data.second << std::endl;
+  data = scope.query(const_cast<ViString>("HORizontal:RECOrdlength?"));
+  std::cout << "RECORD LENGTH: " << data.second << std::endl;
   //scope.binIn();
   //scope.resetScope();
 
@@ -68,105 +70,7 @@ int main()
   scope.stopProfiler("devtest");
 
   return 0;
-}
-/*
-#define MAX_CNT 200 
-
-#define MAX_CNT1 1000
-
-int main(void)
-{
-
-    // VISA variables //
-
-    static unsigned char  strres [1000];
-    static ViStatus status;
-    static ViSession defaultRM, vi;
-    static ViUInt32 retCount;
-    static ViUInt32 actual;
-    //ViChar buffer[MAX_CNT]; 
-
-    // Open session to TCPIP device  //
-
-    viOpenDefaultRM(&defaultRM);
-    viSetAttribute (defaultRM, VI_ATTR_TMO_VALUE,1600);
-    viOpen(defaultRM,"TCPIP::10.176.62.25::INSTR",VI_NULL,VI_NULL,&vi);
-    viSetAttribute (vi, VI_ATTR_TMO_VALUE,100);
-
-    // Data Source & Instrument Identification //
-
-    viWrite(vi, (ViBuf)"SELECT:CH1 ON",14, &actual); 
-    viWrite(vi, (ViBuf)":DATa:SOUrce:AVAILable?",24, &actual);
-    viRead(vi,strres, MAX_CNT1, &retCount);
-    printf("\nTektronix MSO58LP - Receiving  Data\n \n");
-    printf("Data Source Available: %s\n", strres);
-
-    viWrite(vi, (ViBuf)"*IDN?", 6, &actual); 
-    viRead(vi,strres, MAX_CNT, &retCount);
-    printf("Settings Scope: %s\n", strres);
-
-    //////////////// Adquiring Data CH1 ////////////////////
-
-    // Settings //
-
-    viWrite(vi, (ViBuf)":DATa:SOUrce CH1",17, &actual);
-    viRead(vi,strres, MAX_CNT,  &retCount);
-    printf("Receving Data CH1: %s\n", strres);
-
-    viWrite(vi, (ViBuf)":DATa:START 1",14, &actual);
-    viRead(vi,strres, MAX_CNT,  &retCount);
-
-    viWrite(vi, (ViBuf)":DATa:STOP 2500",16, &actual);
-    viRead(vi,strres, MAX_CNT,  &retCount);
-
-    viWrite(vi, (ViBuf)":WFMOutpre:ENCdg BINARY",24, &actual); // Binary Data Receiving
-    viRead(vi,strres, MAX_CNT,  &retCount);
-
-    viWrite(vi, (ViBuf)":WFMOutpre:BYT_Nr 1",20, &actual);
-    viRead(vi,strres, MAX_CNT,  &retCount);
-
-    viWrite(vi, (ViBuf)":HEADer 1",10, &actual);
-    viRead(vi,strres, MAX_CNT,  &retCount);
-
-    viWrite(vi,(ViBuf)":VERBOSE 1", 11,&actual);
-    viRead(vi,strres, MAX_CNT, &retCount);
-
-    // Settings for Curve Query //
-
-    viWrite(vi, (ViBuf)":WFMOutpre?",12, &actual);
-    viRead(vi,strres, MAX_CNT1,  &retCount);
-    printf("%s\n", strres);
-
-    // Query Data //
-
-    viWrite(vi, (ViBuf)":CURVE?",8, &actual);
-    viRead(vi,strres, MAX_CNT1,  &retCount);
-    printf("%s\n ", strres);
-
-    
-    for(int i=14;i<305;i++){
-      std::cout << std::bitset<8>(strres[i]) << " ------ " << (int)strres[i] << std::endl;
-    }
-    
-    // Off Channels//
-
-    viWrite(vi, (ViBuf)"SELECT:CH2 OFF",15, &actual);
-
-    // Close session //
-
-    viClose(vi);
-    viClose(defaultRM);
-    return 0;
 }*/
-
-/*/
-/* TCPIP TEKTRONIX SCOPE BRIL CMS*/
-/*
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <ni-visa/visa.h> 
 
 #define MAX_CNT 200 
 
@@ -177,7 +81,7 @@ int main(void)
 
   // VISA variables //
 
-  static unsigned char  strres [1000];
+  static unsigned char  strres [6263];
   static ViStatus status;
   static ViSession defaultRM, vi;
   static ViUInt32 retCount;
@@ -214,7 +118,7 @@ int main(void)
   viWrite(vi, (ViBuf)":DATa:START 1",14, &actual);
   viRead(vi,strres, MAX_CNT,  &retCount);
 
-  viWrite(vi, (ViBuf)":DATa:STOP 900",16, &actual);
+  viWrite(vi, (ViBuf)":DATa:STOP 6250",16, &actual);
   viRead(vi,strres, MAX_CNT,  &retCount);
 
   viWrite(vi, (ViBuf)":WFMOutpre:ENCdg BINARY",24, &actual); // Binary Data Receiving
@@ -223,7 +127,7 @@ int main(void)
   viWrite(vi, (ViBuf)":WFMOutpre:BYT_Nr 1",20, &actual);
   viRead(vi,strres, MAX_CNT,  &retCount);
 
-  viWrite(vi, (ViBuf)":HEADer 1",10, &actual);
+  viWrite(vi, (ViBuf)":HEADer 0",10, &actual);
   viRead(vi,strres, MAX_CNT,  &retCount);
 
   viWrite(vi,(ViBuf)":VERBOSE 1", 11,&actual);
@@ -241,8 +145,10 @@ int main(void)
   viRead(vi,strres, MAX_CNT1,  &retCount);
   printf("%s\n ", strres);
 
-  for(int i=14;i<305;i++){
-      std::cout << std::bitset<8>(strres[i]) << " ------ " << (int)strres[i] << std::endl;
+  for(int i=14;i<6250;i++){
+      if((int)strres[i] != 0 && (int)strres[i] != 1 && (int)strres[i] != 255){
+        std::cout << std::bitset<8>(strres[i]) << " ------ " << (int)strres[i] << " ------ " << i << std::endl;
+      }
   }
 
   // Off Channels//
@@ -255,4 +161,4 @@ int main(void)
   viClose(defaultRM);
   return 0;
 
-}*/
+}
